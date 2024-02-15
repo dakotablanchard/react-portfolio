9e6537571178dc18d925328022b1d5fb43e7fccc
kota_2699 <dakotablanchard2699@gmail.com>
2024-02-15 13:46:20 -0500

    Updated portfolio page with rooms project

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,20 +4,24 @@ const Portfolio = () => {
     return (
         <div>
             <ProjectLeft
-                image="https://via.placeholder.com/150"
+                image="/assets/rooms.png"
                 title="Rooms"
-                description="This is the first project.
-                It is a simple project that demonstrates the use of HTML, CSS, and JavaScript.
-                It is a simple project that demonstrates the use of HTML, CSS, and JavaScript.
-                It is a simple project that demonstrates the use of HTML, CSS, and JavaScript.
-                It is a simple project that demonstrates the use of HTML, CSS, and JavaScript.
-                It is a simple project that demonstrates the use of HTML, CSS, and JavaScript."
-                siteUrl="https://www.example.com"
-                repoUrl="https://www.github.com"
+                description="
+                Rooms is a dynamic and user-friendly application designed for seamless real-time communication. 
+                Users can easily create an account, join existing rooms, or create their own private spaces to 
+                facilitate live conversations with friends or colleagues. Leveraging the power of WebSocket technology through Socket.io, 
+                the app ensures instantaneous and smooth communication. Built on the robust Express framework and utilizing 
+                Handlebars for efficient templating, Rooms provides an intuitive platform for users to connect, share, 
+                and engage in real-time conversations, making it a versatile and engaging solution for those seeking instant 
+                and interactive communication experiences. The application was created by myself and 3 others, with my focus and main 
+                contributions being back end functionallity. This includes tasks such as server routing, authentication, 
+                and database communication."
+                siteUrl="https://rooms-3226fb27d847.herokuapp.com/login"
+                repoUrl="https://github.com/samanthashleyrose/rooms"
             />
             <ProjectRight
-                image="https://via.placeholder.com/150"
-                title="Rooms"
+                image="/assets/trip-planner.png"
+                title="Automated Trip Planner"
                 description="This is the first project.
                 It is a simple project that demonstrates the use of HTML, CSS, and JavaScript.
                 It is a simple project that demonstrates the use of HTML, CSS, and JavaScript.
@@ -28,8 +32,8 @@ const Portfolio = () => {
                 repoUrl="https://www.github.com"
             />
             <ProjectLeft
-                image="https://via.placeholder.com/150"
-                title="Rooms"
+                image="/assets/blog.png"
+                title="Blog!"
                 description="This is the first project.
                 It is a simple project that demonstrates the use of HTML, CSS, and JavaScript.
                 It is a simple project that demonstrates the use of HTML, CSS, and JavaScript.
