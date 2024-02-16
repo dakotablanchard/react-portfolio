1fe27bce4cdb99d21d3bd1996a92a7ed700dd009
Dakota Blanchard <dakotablanchard2699@gmail.com>
2024-02-16 13:48:46 -0500

    mobile responsiveness on portfolio page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -16,14 +16,14 @@ const ProjectLeft = ({ image, title, description, siteUrl, repoUrl }) => {
 
 const ProjectRight = ({ image, title, description, siteUrl, repoUrl }) => {
   return (
-    <div className="project">
+    <div className="project-right">
+      <img className='port-image' src={image} alt={title} />
       <div className='project-content'>
         <h1>{title}</h1>
         <p id='description'>{description}</p>
         <a id='view-site' href={siteUrl}>View Site</a>
         <a id='github' href={repoUrl}>GitHub Repo</a>
       </div>
-      <img className='port-image' src={image} alt={title} />
     </div>
   );
 }
